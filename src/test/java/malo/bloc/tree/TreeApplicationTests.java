e9d0b58315e0af82b38eb5730597a756dd16a67f
Théophanne Kouchoanou <theophane_malo@yahoo.fr>
2022-03-17 10:31:23 +0100

    commit initial

---
+++ src/test/java/malo/bloc/tree/TreeApplicationTests.java
@@ -0,0 +1,13 @@
+package malo.bloc.tree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
