ce68eae8bb76ea9707640e0699d44e76ee55c8b5
Théophanne Kouchoanou <theophane_malo@yahoo.fr>
2022-03-23 12:39:53 +0100

    add update method and handle associations for save otherwise errors

--- src/main/java/malo/bloc/tree/service/UserService.java
+++ src/main/java/malo/bloc/tree/service/UserService.java
@@ -1,5 +1,7 @@
 package malo.bloc.tree.service;
 
+import malo.bloc.tree.persistence.entity.NodeLeaf;
+import malo.bloc.tree.persistence.entity.Tree;
 import malo.bloc.tree.persistence.entity.User;
 import malo.bloc.tree.persistence.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +13,28 @@ public class UserService {
     private UserRepository userRepository;
 
     public User save(User user){
-        return userRepository.save(user);
+        return userRepository.save(this.handleAssociationForSave(user));
+    }
+
+    public User update(User user){
+        return userRepository.save(this.handleAssociationForSave(user));
     }
 
     public Iterable <User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    public User getUserById(int id){
+        return userRepository.getById(id);
+    }
+    
+    private User handleAssociationForSave(User user){
+        Tree tree = user.getTree();
+        NodeLeaf leaf = tree.getNodeLeaf();
+        tree.setUser(user);
+        leaf.setTree(tree);
+        leaf.getLinks().forEach(l -> l.setLeaf(leaf));
+        leaf.getMetadata().forEach(m-> m.setLeaf(leaf));
+        return user;
     }
 }
