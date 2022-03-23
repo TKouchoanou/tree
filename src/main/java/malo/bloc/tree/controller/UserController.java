1c0ae089e18924df809e7518b49e8f11b9cc1677
Théophanne Kouchoanou <theophane_malo@yahoo.fr>
2022-03-23 12:51:00 +0100

    update controller method use dto, mapper and @Valid annotation with bindingResult. bindingResult must come just after the argument with @Valid annotation

--- src/main/java/malo/bloc/tree/controller/UserController.java
+++ src/main/java/malo/bloc/tree/controller/UserController.java
@@ -1,27 +1,49 @@
 package malo.bloc.tree.controller;
 
+import lombok.SneakyThrows;
+import malo.bloc.tree.dtos.UserDto;
+import malo.bloc.tree.dtos.create.NewUserDto;
+import malo.bloc.tree.mapper.Dto2EntityMapper;
 import malo.bloc.tree.persistence.entity.User;
 import malo.bloc.tree.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    Dto2EntityMapper mapper;
+
     @GetMapping(value = "/users")
     public List<User> users(){
         return (List<User>) userService.getAllUsers();
     }
 
+    @SneakyThrows
     @PostMapping(value = "/user")
-    public User save(@RequestBody User user){
-      return  userService.save(user);
+    public UserDto save(@Valid @RequestBody NewUserDto userDto, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            throw new Exception(""+bindingResult.getFieldErrors()+" "+bindingResult.getFieldErrors());
+        }
+        User user =(User) mapper.toEntity(userDto,User.class);
+        return  (UserDto) mapper.toDto(userService.save(user),UserDto.class);
+    }
+
+    @PutMapping(value = "/user/{id}")
+    public UserDto update(@PathVariable("id") int id, @Valid @RequestBody UserDto userDto, BindingResult bindingResult){
+        if(!Objects.equals(id,userDto.getId())){
+            throw new IllegalArgumentException("User IDs don't match");
+        }
+
+        User user = (User) mapper.toEntity(userDto,User.class);
+        return (UserDto) mapper.toDto(userService.update(user),UserDto.class);
     }
 }
