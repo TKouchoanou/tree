b1fdb34a04f7c99edfe3717049eb331bc87ae56a
Théophanne Kouchoanou <theophane_malo@yahoo.fr>
2022-04-08 14:44:01 +0200

    handle relation of entity for save

--- src/main/java/malo/bloc/tree/mapper/Mapper.java
+++ src/main/java/malo/bloc/tree/mapper/Mapper.java
@@ -6,6 +6,7 @@ import malo.bloc.tree.dtos.create.NewTreeDto;
 import malo.bloc.tree.dtos.create.NewUserDto;
 import malo.bloc.tree.mapper.tree.TreeDto2EntityMapper;
 import malo.bloc.tree.mapper.user.UserDto2EntityMapper;
+import malo.bloc.tree.persistence.entity.NodeLeaf;
 import malo.bloc.tree.persistence.entity.Tree;
 import malo.bloc.tree.persistence.entity.User;
 import org.modelmapper.ModelMapper;
@@ -20,7 +21,8 @@ public class Mapper implements TreeDto2EntityMapper, UserDto2EntityMapper {
 
     @Override
     public User toEntity(UserDto o) {
-        return this.modelMapper.map( o,User.class);
+        User user = this.modelMapper.map( o,User.class);
+        return handleUserAssociationForSave(user);
     }
 
     @Override
@@ -30,13 +32,15 @@ public class Mapper implements TreeDto2EntityMapper, UserDto2EntityMapper {
 
     @Override
     public User toEntity(NewUserDto o) {
-        return this.modelMapper.map( o,User.class);
+        User user = this.modelMapper.map( o,User.class);
+        return handleUserAssociationForSave(user);
     }
 
 
     @Override
     public Tree toEntity(NewTreeDto o) {
-        return this.modelMapper.map( o,Tree.class);
+        Tree tree = this.modelMapper.map( o,Tree.class);
+        return handleTreeAssociationForSave(tree);
     }
 
     @Override
@@ -47,6 +51,30 @@ public class Mapper implements TreeDto2EntityMapper, UserDto2EntityMapper {
 
     @Override
     public Tree toEntity(TreeDto o) {
-        return this.modelMapper.map( o,Tree.class);
+        Tree tree = this.modelMapper.map( o,Tree.class);
+        return handleTreeAssociationForSave(tree);
+    }
+
+
+    private User handleUserAssociationForSave(User user){
+        Tree tree = user.getTree();
+        if(tree!=null)
+            handleTreeAssociationForSave(tree.setUser(user));
+        return user;
+    }
+
+    private Tree handleTreeAssociationForSave(Tree tree){
+        NodeLeaf leaf = tree.getNodeLeaf();
+        if(leaf!=null)
+            handleLeafAssociationForSave(leaf.setTree(tree));
+            tree.getChildren().forEach(child -> child.setParent(tree));
+            tree.getChildren().forEach(this::handleTreeAssociationForSave);
+        return tree;
+    }
+
+    private NodeLeaf handleLeafAssociationForSave(NodeLeaf leaf){
+        leaf.getLinks().forEach(l -> l.setLeaf(leaf));
+        leaf.getMetadata().forEach(m-> m.setLeaf(leaf));
+        return leaf;
     }
 }
